3c20006c3d4f84055fc3711c79fc72b6e123db01
r3850355 <r3850355@gmail.com>
2023-01-06 15:34:32 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# image-cache-test
+
+This template should help get you started developing with Vue 3 in Vite.
+
+```
+npm i
+npm run dev
+```
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+// import TheWelcome from '../components/TheWelcome.vue'
+let random = ref(0)
+random.value = Math.floor(Math.random() * 99999)
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <h1>Home Page</h1>
+    <img src="https://vitejs.dev/logo-with-shadow.png" style="width:150px">
+    <img :src="`https://vitejs.dev/logo-with-shadow.png?something=${random}`" style="width:150px">
+    <img src="https://vitejs.dev/logo-with-shadow.png?otherthing=123" style="width:150px">
+  </main>
+</template>
